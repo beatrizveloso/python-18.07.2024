f919b03858922c8b0dead8615d1916617b6c8b27
beatrizveloso <beatrizoslov6@gmail.com>
2024-07-18 16:25:01 -0300

    second commit

--- PLANILHA-18.07.py
+++ PLANILHA-18.07.py
@@ -1,5 +1,5 @@
 import pandas as pd
-excel = pd.read_excel("AULA 10 - 18_07 - PYTHON.xlsx")
+excel = pd.read_excel("vendedor.xlsx")
 
 # Variável 'mês' para controlar o fluxo de venda dos meses
 mes = input("Qual mês você quer analisar? ")
@@ -7,4 +7,17 @@ mes = input("Qual mês você quer analisar? ")
 # Retornar análise do vendedor do mês
 # Ordena o mês
 analise = excel.sort_values(by=f'{mes}',ascending=False)
-print(analise)
+# Melhorando...
+print(f"O melhor vendedor do mês {mes}, foi a {analise.iloc[0][0]}")
+
+# Quem menos vendeu
+analise = excel.sort_values(by=f'{mes}',ascending=True)
+# ...
+print(f"O pior vendedor do mês {mes}, foi a {analise.iloc[0][0]}")
+
+
+# Só quero o vendedor, como ele está na primeira coluna: [0][0]
+print(analise.iloc[0][0])
+
+
+
